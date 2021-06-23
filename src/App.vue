f63c79043c77a136deb3225ad15f2ee65f40270f
Mwangi Kabiru <mwangibrian8@gmail.com>
2021-06-23 21:52:21 +0300

    Added map, geo-locate, and simple marker

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GmapMap
+      :center="center"
+      :zoom="15"
+      map-style-id="roadmap"
+      :options="mapOptions"
+      style="width: 100vmax; height: 80vmin"
+      ref="mapRef"
+      @click="handleMapClick"
+    >
+      <GmapMarker
+        :position="marker.position"
+        :clickable="true"
+        :draggable="true"
+        @drag="handleMarkerDrag"
+        @click="panToMarker"
+      />
+    </GmapMap>
+  <button @click="geolocate">Detect Location</button>
+
+  <p>Selected Position: {{ marker.position }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data() {
+    return {
+      marker: { position: { lat: 1.2921, lng: 36.8219 } },
+      center: { lat: 1.2921, lng: 36.8219 },
+
+      mapOptions: {
+        disableDefaultUI: true,
+      },
+    };
+  },
+  methods: {
+    //detects location from browser
+    geolocate() {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.marker.position = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        }
+
+        this.panToMarker();
+      });
+    },
+
+    //sets the position of marker when dragged
+    handleMarkerDrag(e) {
+      this.marker.position = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+    },
+
+    //Moves the map view port to marker
+    panToMarker() {
+      this.$refs.mapRef.panTo(this.marker.position);
+      this.$refs.mapRef.setZoom(15);
+    },
+
+    //Moves the marker to click position on the map
+    handleMapClick(e) {
+      this.marker.position = { lat: e.latLng.lat(), lng: e.latLng.lng() };
+      console.log(e);
+    },
+  },
+  mounted() {
+    this.geolocate();
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from "vue2-google-maps";
 
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyA4flObfJL9BV2W__93h9rsnHhH3segtBA",
+  },
+  installComponents: true,
+});
 Vue.config.productionTip = false
 
 new Vue({
