a65c1a1efdcbb489f2f43930b5cf25ecd991c4ef
Mwangi Kabiru <mwangibrian8@gmail.com>
2021-06-25 00:14:33 +0300

    Added instructions and name, re-factoring

--- src/App.vue
+++ src/App.vue
@@ -33,23 +33,25 @@
                   @click="simulateDriverA"
                 >
                   <v-list-item-content>
-                    <v-list-item-title>
+                    <v-list-item-title style="color: green">
                       Simulate Scenario
                     </v-list-item-title>
                   </v-list-item-content>
                 </v-list-item>
 
-                <!-- <v-list-item
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item
                   link
                   color="grey lighten-4"
-                  @click="refreshSimulation"
                 >
                   <v-list-item-content>
-                    <v-list-item-title>
-                      Refresh
-                    </v-list-item-title>
+                    <p>
+                      Click "simulate scenario" to play out the scenario from beginning to end. <br /><br />
+                      By Brian Mwangi Kabiru
+                    </p>
                   </v-list-item-content>
-                </v-list-item> -->
+                </v-list-item>
               </v-list>
             </v-sheet>
           </v-col>
